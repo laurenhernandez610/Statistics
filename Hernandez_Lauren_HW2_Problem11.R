8a14215680e3e5d619104a80488a9dfae1e4923d
Lauren Hernandez <lauren.hernandez.m@gmail.com>
2021-07-28 14:20:14 -0500

    First commit

---
+++ Hernandez_Lauren_HW2_Problem11.R
@@ -0,0 +1,12 @@
+# Lauren Hernandez 
+# Math 3339
+# Homework 2
+# Problem h
+
+# Find the CDF of X
+
+library(data.table)
+library(mltools)
+
+dt <- data.table(x=c(-3,-1,-2,0,1,1.2,2,3,4), y=c(0.12,0.11,0.11,0.09,0.13,0.09,0.11,0.12,0.12))
+
